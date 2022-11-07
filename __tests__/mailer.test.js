49c75f3ef9dd904e6cdc8377e55bdc87ff75636c
EunhaLim <dmsgk1559@gmail.com>
2022-11-07 18:26:26 +0900

    rename __test__ -> __tests__

---
+++ __tests__/mailer.test.js
@@ -0,0 +1,16 @@
+import { jest } from '@jest/globals';
+import request from 'supertest';
+
+/**
+ * send: token, emailTitle, emailbody(userName, button with token and userId)
+ * apply: check token by user
+ */
+describe('mailsender', () => {
+  test('send mail', () => {
+
+  });
+
+  test('confirm token', () => {
+
+  });
+});
